0272dce74199cbc07e82883524cc63f6154c3d92
KishiEdward <dzidanrh@gmail.com>
2026-03-17 13:15:54 +0700

    membuat funcion init

--- config/firebase.go
+++ config/firebase.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/KishiEdward/backend_ginApp/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	
+}
