050314cfce354ca1ad31cbd03d6db7b1421212dd
KishiEdward <dzidanrh@gmail.com>
2026-03-17 13:16:46 +0700

    mengambil database

--- config/firebase.go
+++ config/firebase.go
@@ -14,5 +14,11 @@ import (
 var DB *gorm.DB
 
 func InitDatabase() {
-	
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
 }
