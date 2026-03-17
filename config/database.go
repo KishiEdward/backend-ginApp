48245d66327ae6dad8fee348662395856ab89550
KishiEdward <dzidanrh@gmail.com>
2026-03-17 13:09:19 +0700

    membuat function database

--- config/database.go
+++ config/database.go
@@ -10,3 +10,13 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
+
+var DB *gorm.DB
+
+func Database() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+}
