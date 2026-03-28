7b96eb31c09efc98d0a54e52c2e6b482c9fa1d21
KishiEdward <dzidanrh@gmail.com>
2026-03-28 17:34:54 +0700

    feat(model): add name and role columns to User

--- models/user.go
+++ models/user.go
@@ -6,4 +6,6 @@ type User struct {
 	gorm.Model
 	FirebaseUID   string `gorm:"uniqueIndex;size:128;not null" json:"firebase_uid"`
 	Email         string `gorm:"uniqueIndex;size:255;not null" json:"email"`
+	Name          string `gorm:"size:100" json:"name"`
+	Role          string `gorm:"size:20;default:user" json:"role"`
 }
