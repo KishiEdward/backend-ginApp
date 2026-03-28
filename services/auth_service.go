02ee042e0b12e14cae465ae301e1d6bf31404afd
KishiEdward <dzidanrh@gmail.com>
2026-03-28 18:04:54 +0700

    feat(service): add helper to generate backend JWT

--- services/auth_service.go
+++ services/auth_service.go
@@ -21,3 +21,24 @@ type AuthService struct {
 func NewAuthService() *AuthService {
 	return &AuthService{userRepo: repositories.NewUserRepository()}
 }
+
+func (s *AuthService) generateJWT(user *models.User) (string, error) {
+	expireHours, _ := strconv.Atoi(os.Getenv("JWT_EXPIRE_HOURS"))
+	if expireHours == 0 {
+		expireHours = 24
+	}
+
+	claims := jwt.MapClaims{
+		"sub":            user.ID,
+		"firebase_uid":   user.FirebaseUID,
+		"email":          user.Email,
+		"name":           user.Name,
+		"role":           user.Role,
+		"iat":            time.Now().Unix(),
+		"exp":            time.Now().Add(time.Hour * time.Duration(expireHours)).Unix(),
+		"email_verified": user.EmailVerified,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
