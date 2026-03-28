a1803902c4252e3bd75ecb46e41520688612274e
KishiEdward <dzidanrh@gmail.com>
2026-03-28 17:33:52 +0700

    feat(model): add firebase uid and email columns to User

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
+
 import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	FirebaseUID   string `gorm:"uniqueIndex;size:128;not null" json:"firebase_uid"`
+	Email         string `gorm:"uniqueIndex;size:255;not null" json:"email"`
 }
